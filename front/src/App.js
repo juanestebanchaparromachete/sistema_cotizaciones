bc8091fcfd650fe1b876b867a7b08132240a8c9a
a-cordoba10 <icoltus@hotmail.com>
2017-09-28 17:38:45 -0500

    WQEFEWF
    WQFWEQF

--- front/src/App.js
+++ front/src/App.js
@@ -1,118 +1,48 @@
-<<<<<<< HEAD:front/src/components/App/index.js
-import React, {
-    Component
-}
-from 'react';
-import './styleApp.css';
-import { createQuote } from './createQuote'
-=======
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 //import { createBlogPost } from './createProduct'
 
->>>>>>> parent of 93a5c8f... routing:front/src/App.js
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            products: []
-            , quote: { email: '', telefono: '', 'fecha': new Date(), nombreCliente: '', productos:[]},
-            // productos: [],
-            condition: false
-        };
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this);
-        this.addProduct = this.addProduct.bind(this);
-        this.nameChange = this.nameChange.bind(this);
-        this.emailChange = this.emailChange.bind(this);
-        this.phoneChange = this.phoneChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleClick() {
-        this.setState( { condition : !this.state.condition } ); 
-    }
-    addProduct(prod) {
-        const newPro = this.state.quote.productos.concat(prod);
-        this.state.quote.productos = newPro;
-        this.setState(prevState => ({
-            quote: this.state.quote
-        }));
-        console.log(this.state.quote);
-    }
-    nameChange(event) {
-    const target = event.target;
-    const value = target.value;
-    this.state.quote.nombreCliente = target.value;
-        this.setState(prevState => ({
-             quote: this.state.quote
-        }));
-    }
-    emailChange(event) {
-    const target = event.target;
-    const value = target.value;
-    this.state.quote.email = target.value;
-        this.setState(prevState => ({
-             quote: this.state.quote
-        }));
-    }
-    phoneChange(event) {
-    const target = event.target;
-    const value = target.value;
-    this.state.quote.telefono = target.value;
-        this.setState(prevState => ({
-             quote: this.state.quote
-        }));
-    }
-    componentDidMount() {
-            fetch('/products').then(res => res.json()).then(products => this.setState({
-                products
-            }));
-        }
-    handleSubmit() {
-                createQuote(this.state.quote);
-            }
-    render() {
-        return ( 
-            
-            < div id = "products" > 
-                <div className={this.state.condition ? "quoteBG active" :"quoteBG"}>
-                    <div className={this.state.condition ? "quote active" :"quote"}>
-                        <h2>Tu carrito de compras</h2>
-                        <form onSubmit={this.handleSubmit}>
-                            <label>
-                            Nombre:<input required type="text" value={this.state.quote.nombre} onChange={this.nameChange} />
-                            </label>
-                            <label>
-                            Email:<input required type="email" value={this.state.quote.email} onChange={this.emailChange} />
-                            </label>
-                            <label>
-                            Telefono:<input required type="number" value={this.state.quote.telefono} onChange={this.phoneChange} />
-                            </label>
-                            <button type="submit">Enviar cotizaci&oacute;n</button>
-                        </form> 
-                       { this.state.quote.productos.map((objProd) => < div key = {objProd.urlImagen}
-                        className = "review" > < img src = {objProd.urlImagen}/> < h2 > {objProd.nombre} </h2>  </div>)}
-                    </div>
-                </div>
-                < button onClick ={this.handleClick.bind(this)}
-            className={this.state.condition ? "shoppingCart active" :"shoppingCart"} > </button> {
-                this.state.products.map(product => < div key = {
-                        product.id
-                    }
-                    className = "product" > < img src = {
-                        product.urlImagen
-                    }
-                    /> < h2 > {
-                        product.nombre
-                    } </h2> < h3 > {
-                        product.descripcion
-                    } </h3> < h4 > < b > alto: </b>{product.alto} m < b > ancho: </b>{product.ancho} m < b > profundo: </b>{product.largo} m </h4> < button className = "addProduct"
-                    onClick = {() => {this.addProduct(product)}} > Agregar a Cotizaci&oacute;n </button> </div>)
-            } </div>);
-    }
+    
+state = {products: []}
+
+ componentDidMount() {
+   fetch('/products')
+     .then(res => res.json())
+     .then(products => this.setState({ products }));
+ }
+
+//   handleSubmit() {
+//        createBlogPost();
+//    }
+
+ render() {
+   return (
+     <div id="products">
+       {this.state.products.map(product =>
+       
+        <div key={product.id} className="product">
+            <img src={product.urlImagen} />
+            <h2>{product.nombre}</h2>
+            <h3>{product.descripcion}</h3>
+            <h4>
+                <b>alto:</b>{product.alto}
+                <b>ancho:</b>{product.ancho}
+                <b>profundo:</b>{product.largo}
+            </h4>
+        </div>
+       
+       )}
+
+
+     </div>
+
+   );
+ }
 }
+
 export default App;
 //         <div>
 //                <button onClick={this.handleSubmit}>Handle</button>
